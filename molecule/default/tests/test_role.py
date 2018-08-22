5fb458fada9a1c9dd86ce192dbf5ce1e1f36e3a5
John Freeman <john.freeman.opensource@gmail.com>
2018-08-22 14:57:40 +0000

    Changed tests to use host fixture
    Other fixtures are deprecated and will be removed in 2.0 Testinfra release.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_molecule(Command):
+def test_molecule(host):
 
     # Check the `molecule` executes successfully.
-    assert Command('molecule --version').rc == 0
+    assert host.run('molecule --version').rc == 0
